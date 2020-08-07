115f7143b9bf3b5ca565a98c3bae387f5d717e75
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-08-07 20:42:53 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -32,6 +32,11 @@ class Transfer
   end
   
   def reverse_transfer
-    
+    if sender.status = "open" && receiver.status == "open" && amount < receiver.balance && self.status == "complete"
+      sender.balance = sender.balance + amount
+      receiver.balance = receiver.balance - amount
+      self.status = "reversee"
+    end
+  end
   
 end
