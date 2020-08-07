7d2eaed59003e3377bfd7b898ae688979736485f
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-08-07 19:42:33 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,9 +1,13 @@
 class Transfer
   
-  attr_accessor :transfer 
+  attr_accessor :transfer, :status, :sender, :receiver, :transfer_amount
   
-  def initialize(transfer)
-    @transfer = transfer 
+  def initialize(transfer, transfer_amount = 0)
+    @transfer = transfer
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @transfer_amount = transfer_amount
   end
   
 end
